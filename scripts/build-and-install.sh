e1688a9befbe65d4b28c9c6a919f6a1194b17675
Silvio Cesare <silvio.cesare@gmail.com>
2013-02-12 14:59:20 +1100

    script to build
    config

---
+++ scripts/build-and-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf ~/Clonewise
+cd ~
+apt-get install git openmpi-bin libopenmpi-dev g++ make libxerces-c-dev libxerces-c3.1 libfuzzy-dev
+git clone http://github.com/siliocesare/Clonewise.git
+cd ~/Clonewise
+make
+sudo make install

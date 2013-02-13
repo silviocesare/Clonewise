f1b2fce09c9425fc2b8c7b7783095c6da65d3136
Silvio Cesare <silvio.cesare@gmail.com>
2013-02-13 16:06:12 +1100

    getting ready for debian packages

---
+++ scripts/build-debian-packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -rf ~/Clonewise
+cd ~
+apt-get install git openmpi-bin libopenmpi-dev g++ make libxerces-c-dev libxerces-c3.1 libfuzzy-dev weka
+git clone http://github.com/siliocesare/Clonewise.git
+VERSION="$(cat ~/Clonewise/VERSION)"
+mv ~/Clonewise ~/Clonewise-$VERSION
+tar czvf ~/Clonewise-$VERSION.tar.gz ~/Clonewise-$VERSION
+cp -R ~/Clonewise-$VERSION ~/Clonewise-configs-$VERSION
+mv ~/Clonewise-$VERSION ~/Clonewise-bins-$VERSION
+cd ~/Clonewise-bins-$VERSION
+dh_make -f ../$PACKAGENAME.tar.gz
+mkdir debian/patches
+cp ~/Clonewise-configs-$VERSION/patches/debian-install-bins.patch debian/patches
+cd ~/Clonewise-configs-$VERSION
+dh_make -f ../$PACKAGENAME.tar.gz
+mkdir debian/patches
+cp ~/Clonewise-configs-$VERSION/patches/debian-install-configs.patch debian/patches

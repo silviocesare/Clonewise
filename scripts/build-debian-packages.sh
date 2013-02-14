e3033dc696c416d49e3d5e9658ba9aa132996534
Silvio Cesare <silvio.cesare@gmail.com>
2013-02-14 11:01:29 +1100

    build package

--- scripts/build-debian-packages.sh
+++ scripts/build-debian-packages.sh
@@ -12,13 +12,17 @@ mv ~/Clonewise ~/Clonewise-$VERSION
 tar czvf ~/Clonewise-$VERSION.tar.gz ~/Clonewise-$VERSION
 cp -R ~/Clonewise-$VERSION ~/clonewise-configs-$VERSION
 cp -R ~/Clonewise-$VERSION ~/clonewise-bins-$VERSION
+
 cd ~/clonewise-bins-$VERSION
 dh_make -s -f ../Clonewise-${VERSION}.tar.gz --copyright gpl3
 mkdir debian/patches
 cp ~/clonewise-configs-${VERSION}/patches/debian-install-bins.patch debian/patches
+dpkg-buildpackage
+
 cd ~/clonewise-configs-$VERSION
 dh_make -i -f ../Clonewise-${VERSION}.tar.gz --copyright gpl3
 mkdir debian/patches
 cp ~/clonewise-configs-${VERSION}/patches/debian-install-configs.patch debian/patches
 echo config/clones/weka/model >> debian/source/include-binaries
 echo config/clones/weka/model2 >> debian/source/include-binaries
+dpkg-buildpackage
